9d106dfb9194500f3a54c291e429bab7497e6137
Yong Wang <javatmn@gmail.com>
2019-06-05 16:06:27 +0800

    Dialog for switching added

--- lib/main.dart
+++ lib/main.dart
@@ -3,110 +3,119 @@ import 'dart:async';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-void main() {
-  runApp(new MaterialApp(
-    home: new HomePage(),
-  ));
-}
+void main() => runApp(MaterialApp(home: HomePage()));
 
-enum Answers { YES, NO, MAYBE }
+enum Answers { UDP, TCP, NONE }
 
-// Create a stateful widget
+/*
+**  Home page
+*/
 class HomePage extends StatefulWidget {
   @override
   HomePageState createState() => new HomePageState();
 }
 
-// Create the state for our stateful widget
+/*
+**  Home page state
+*/
 class HomePageState extends State<HomePage> {
-  final String url = "http://javatmn.us.to:1236/get_server_list";
-  List serverList;
-  bool _lights = true;
-  String _value = '';
+  final String URL_GET_SERVER_LIST =
+      "http://javatmn.us.to:1236/get_server_list";
+  List _serverList;
+  String _protocol = 'udp';
 
-  void _setValue(String value) => setState(() => _value = value);
+  void _setValue(String value) => setState(() => _protocol = value);
 
-  // Function to get the JSON data
+  /*
+  **  Get server list using RESTful API
+  */
   Future<String> get_server_list() async {
-    var response = await http
-        .get(Uri.encodeFull(url), headers: {"Accept": "application/json"});
+    var response = await http.get(Uri.encodeFull(URL_GET_SERVER_LIST),
+        headers: {"Accept": "application/json"});
 
     print(response.body);
 
     setState(() {
-      serverList = json.decode(response.body);
+      _serverList = json.decode(response.body);
     });
 
     return "Successfull";
   }
 
-  Future _askUser() async {
+  /*
+  **  switch server dialog
+  */
+  Future<void> switch_user_dialog() async {
     switch (await showDialog(
         context: context,
-        /*it shows a popup with few options which you can select, for option we
-        created enums which we can use with switch statement, in this first switch
-        will wait for the user to select the option which it can use with switch cases*/
         builder: (BuildContext context) {
           return SimpleDialog(
-            title: new Text('VPS切换(可能有短暂断流)'),
+            title: Text('VPS切换(会有短暂断流)'),
             children: <Widget>[
               ListTile(
-                leading: Icon(Icons.format_underlined, color: Colors.green,size: 48.0,),
+                leading: Icon(
+                  Icons.format_underlined,
+                  color: Colors.green,
+                  size: 48.0,
+                ),
                 title: Text('UDP模式'),
                 subtitle: Text('推荐'),
                 onTap: () {
-                  Navigator.pop(context, Answers.YES);
+                  Navigator.pop(context, Answers.UDP);
                 },
               ),
               ListTile(
-                leading: Icon(Icons.title, color: Colors.green,size: 48.0,),
+                leading: Icon(
+                  Icons.title,
+                  color: Colors.green,
+                  size: 48.0,
+                ),
                 title: Text('TCP模式'),
                 subtitle: Text('当UDP无效时使用'),
                 onTap: () {
-                  Navigator.pop(context, Answers.NO);
+                  Navigator.pop(context, Answers.TCP);
                 },
               ),
               ListTile(
-                leading: Icon(Icons.clear, color: Colors.red,size: 48.0,),
+                leading: Icon(
+                  Icons.clear,
+                  color: Colors.red,
+                  size: 48.0,
+                ),
                 title: Text('取消'),
                 onTap: () {
-                  Navigator.pop(context, Answers.MAYBE);
+                  Navigator.pop(context, Answers.NONE);
                 },
               ),
             ],
           );
         })) {
-      case Answers.YES:
-        _setValue('Yes');
+      case Answers.UDP:
+        _setValue('udp');
         break;
-      case Answers.NO:
-        _setValue('No');
+      case Answers.TCP:
+        _setValue('tcp');
         break;
-      case Answers.MAYBE:
-        _setValue('Maybe');
+      case Answers.NONE:
         break;
     }
-    print(_value);
+    print(_protocol);
   }
 
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text("Tbox VPN Controller"),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("TBox VPN Controller"),
       ),
-      // Create a Listview and load the data when available
-      body: new ListView.builder(
-          itemCount: serverList == null ? 0 : serverList.length,
+      body: ListView.builder(
+          itemCount: _serverList == null ? 0 : _serverList.length,
           itemBuilder: (BuildContext context, int index) {
-            var server = serverList[index];
+            var server = _serverList[index];
             var color = server['active'] == true ? Colors.red : Colors.green;
-            String btnText = server['active'] == true
-                ? '  Change Protocol  '
-                : 'Switch To This VPS';
-            return new Column(
+            return Column(
               children: <Widget>[
-                new Card(
+                Card(
                   child: Column(
                     children: <Widget>[
                       ListTile(
@@ -114,46 +123,13 @@ class HomePageState extends State<HomePage> {
                           Icons.computer,
                           color: color,
                         ),
-                        title: Text(serverList[index]['ip'] +
-                            '  -  ' +
-                            server['location']),
+                        title:
+                            Text(server['ip'] + '  -  ' + server['location']),
                         subtitle:
                             Text('Clients: ' + server['clients'].toString()),
                         trailing: Icon(Icons.more_vert),
-                        onTap: _askUser,
+                        onTap: switch_user_dialog,
                       ),
-                      /*
-                      ButtonTheme.bar(
-                        //height: 30,
-                        // make buttons use the appropriate styles for cards
-                        child: ButtonBar(
-                          children: <Widget>[
-                            DropdownButton<String>(
-                              value: dropdownValue,
-                              onChanged: (String newValue) {
-                                setState(() {
-                                  dropdownValue = newValue;
-                                });
-                              },
-                              items: <String>[
-                                'UDP',
-                                'TCP'
-                              ].map<DropdownMenuItem<String>>((String value) {
-                                return DropdownMenuItem<String>(
-                                  value: value,
-                                  child: Text(value),
-                                );
-                              }).toList(),
-                            ),
-                            FlatButton(
-                              child: Text(btnText),
-                              onPressed: () {
-                                print(serverList[index]['ip']);
-                              },
-                            ),
-                          ],
-                        ),
-                      ),*/
                     ],
                   ),
                 )
@@ -166,8 +142,6 @@ class HomePageState extends State<HomePage> {
   @override
   void initState() {
     super.initState();
-
-    // Call the getJSONData() method when the app initializes
     this.get_server_list();
   }
 }
