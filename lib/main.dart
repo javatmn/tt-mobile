eb37ef74b8e45093be2a48995a1b4796cd0ae844
Yong Wang <javatmn@gmail.com>
2019-06-10 17:29:47 +0800

    switch server improvement

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'dart:convert';
 
 void main() => runApp(MaterialApp(home: HomePage()));
 
-enum Answers { UDP, TCP, NONE }
+enum Protocols { UDP, TCP, NONE }
 
 /*
 **  Home page
@@ -22,52 +22,83 @@ class HomePageState extends State<HomePage> {
   final String URL_GET_SERVER_LIST =
       "http://javatmn.us.to:1236/get_server_list";
   final String URL_SWITCH_SERVER = "http://javatmn.us.to:1236/switch_server";
-  final style = TextStyle(color: Colors.blueAccent);
+  final blueTxtStyle = TextStyle(color: Colors.blueAccent);
   final iconMoreVert = Icon(Icons.more_vert);
-  final iconPlay = Icon(Icons.check_box_outline_blank);
+  final iconFwd = Icon(Icons.arrow_forward);
+
   List _serverList;
   String _protocol = 'udp';
+  int _selectedTab = 0;
 
-  void _setValue(String value) => setState(() => _protocol = value);
+  List<Widget> _tabs = <Widget>[
+    Text(
+      'Connecting to the BOX, please wait...',
+    ),
+    Text(
+      'Index 1: TODO Stats',
+    ),
+    Text(
+      'Index 2: TODO Settings',
+    ),
+  ];
+
+  void _setProtocol(String value) => setState(() => _protocol = value);
+
+  void _setSelectedTab(int index) => setState(() => _selectedTab = index);
+
+  void _setServerList(List list) => setState(() {
+        _serverList = list;
+        _tabs[0] = _buildVpsTab();
+      });
 
   /*
-  **  Get server list using RESTful API
+  **  Get server list using REST API
   */
-  Future<String> get_server_list() async {
-    var response = await http.get(Uri.encodeFull(URL_GET_SERVER_LIST),
-        headers: {"Accept": "application/json"});
+  Future<int> get_server_list() async {
+    var response;
+
+    try {
+      response = await http.get(Uri.encodeFull(URL_GET_SERVER_LIST),
+          headers: {"Accept": "application/json"});
+    } catch (e) {
+      print(e);
+      return 1;
+    }
 
     print(response.body);
+    _setServerList(json.decode(response.body));
 
-    setState(() {
-      _serverList = json.decode(response.body);
-    });
-
-    return "Successfull";
+    return 0;
   }
 
   /*
-  **  switch server using RESTful API
+  **  switch server using REST API
   */
-  Future<String> switch_server(server) async {
+  Future<int> switch_server(server) async {
     var req = {
       "ip": server['ip'],
       "port": server['port'],
       "lmsPort": server['lmsPort'],
       "protocol": _protocol,
     };
-    var response = await http.post(Uri.encodeFull(URL_SWITCH_SERVER),
-        body: json.encode(req));
+    var response;
+
+    try {
+      response = await http.post(Uri.encodeFull(URL_SWITCH_SERVER),
+          body: json.encode(req));
+    } catch (e) {
+      print(e);
+      return 1;
+    }
 
     print(response.body);
-
-    return "Successfull";
+    return 0;
   }
 
   /*
   **  switch server dialog
   */
-  Future<void> switch_user_dialog(server) async {
+  Future<void> switch_server_dialog(server) async {
     switch (await showDialog(
         context: context,
         builder: (BuildContext context) {
@@ -82,13 +113,13 @@ class HomePageState extends State<HomePage> {
                 ),
                 title: Text(
                   'UDP模式',
-                  style: style,
+                  style: blueTxtStyle,
                 ),
                 subtitle: Text('推荐'),
                 onTap: () {
-                  Navigator.pop(context, Answers.UDP);
+                  Navigator.pop(context, Protocols.UDP);
                 },
-                trailing: iconPlay,
+                trailing: iconFwd,
               ),
               ListTile(
                 leading: Icon(
@@ -98,13 +129,13 @@ class HomePageState extends State<HomePage> {
                 ),
                 title: Text(
                   'TCP模式',
-                  style: style,
+                  style: blueTxtStyle,
                 ),
                 subtitle: Text('推荐UDP无效时使用'),
                 onTap: () {
-                  Navigator.pop(context, Answers.TCP);
+                  Navigator.pop(context, Protocols.TCP);
                 },
-                trailing: iconPlay,
+                trailing: iconFwd,
               ),
               ListTile(
                 leading: Icon(
@@ -114,68 +145,97 @@ class HomePageState extends State<HomePage> {
                 ),
                 title: Text(
                   '取消',
-                  style: style,
+                  style: blueTxtStyle,
                 ),
                 onTap: () {
-                  Navigator.pop(context, Answers.NONE);
+                  Navigator.pop(context, Protocols.NONE);
                 },
-                trailing: iconPlay,
+                trailing: iconFwd,
               ),
             ],
           );
         })) {
-      case Answers.UDP:
-        _setValue('udp');
-        //switch_server(ip, port, lmsPort)
+      case Protocols.UDP:
+        _setProtocol('udp');
         break;
-      case Answers.TCP:
-        _setValue('tcp');
+      case Protocols.TCP:
+        _setProtocol('tcp');
         break;
-      case Answers.NONE:
+      case Protocols.NONE:
         return;
         break;
+      default:
+        return;
     }
     print(_protocol);
     switch_server(server);
   }
 
+  Widget _buildVpsTab() {
+    return ListView.builder(
+        itemCount: _serverList == null ? 0 : _serverList.length,
+        itemBuilder: (BuildContext context, int index) {
+          var server = _serverList[index];
+          var color = server['active'] == true ? Colors.red : Colors.green;
+          return Column(
+            children: <Widget>[
+              Card(
+                child: Column(
+                  children: <Widget>[
+                    ListTile(
+                      leading: Icon(
+                        Icons.computer,
+                        color: color,
+                      ),
+                      title: Text(server['ip'] + '  -  ' + server['location']),
+                      subtitle:
+                          Text('Clients: ' + server['clients'].toString()),
+                      trailing: Icon(Icons.more_vert),
+                      onTap: () {
+                        switch_server_dialog(server);
+                      },
+                      selected: server['active'] == true,
+                    ),
+                  ],
+                ),
+              )
+            ],
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
+    final snackConnectFail = () => Scaffold.of(context).showSnackBar(
+          SnackBar(
+            content: Text('Box connection fail'),
+            duration: Duration(seconds: 3),
+          ),
+        );
     return Scaffold(
       appBar: AppBar(
         title: Text("TBox VPN Controller"),
       ),
-      body: ListView.builder(
-          itemCount: _serverList == null ? 0 : _serverList.length,
-          itemBuilder: (BuildContext context, int index) {
-            var server = _serverList[index];
-            var color = server['active'] == true ? Colors.red : Colors.green;
-            return Column(
-              children: <Widget>[
-                Card(
-                  child: Column(
-                    children: <Widget>[
-                      ListTile(
-                        leading: Icon(
-                          Icons.computer,
-                          color: color,
-                        ),
-                        title:
-                            Text(server['ip'] + '  -  ' + server['location']),
-                        subtitle:
-                            Text('Clients: ' + server['clients'].toString()),
-                        trailing: Icon(Icons.more_vert),
-                        onTap: () {
-                          switch_user_dialog(server);
-                        },
-                        selected: server['active'] == true,
-                      ),
-                    ],
-                  ),
-                )
-              ],
-            );
-          }),
+      body: _tabs.elementAt(_selectedTab),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            title: Text('VPS'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.show_chart),
+            title: Text('Stats'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            title: Text('Settings'),
+          ),
+        ],
+        currentIndex: _selectedTab,
+        selectedItemColor: Colors.amber[800],
+        onTap: _setSelectedTab,
+      ),
     );
   }
 
